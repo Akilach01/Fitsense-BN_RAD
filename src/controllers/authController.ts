fbd8803c2db556affcc4b4e43abe1e6424dabd35
Akilach01 <akilapeiris92@gmail.com>
2025-12-05 14:49:06 +0530

    register function completed

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,7 @@
 import user from "../models/user";
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import user from "../models/user";
 
 
 
@@ -8,13 +10,26 @@ export const register = async(req:Request,res:Response)=>{
     try {
         const {name,email,password,role} =req.body;
 
-        const existingUser = await user.findOne({email});
+        //check user
+
+        const existingUser = await User.findOne({email});
         if(existingUser)
             return res.status(400).json({message:"already have an acount with this email"});
 
-        const hashedPassword = await bcrypt.hash
-        
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({name,email,password:hashedPassword,
+            role:role || "user",  //default normal user
+        });
+
+        return res.status(200).json({
+            message:"user registered succesfully",
+            user:{id:user._id, name:user.name, email:user.email,role:user.role }
+        });
+
     } catch (error) {
-        
-    }
-}
+        return res.status(500).json({message:"server error", error});
+        }
+};
+
+
