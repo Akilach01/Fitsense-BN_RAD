28ac1ee56bf19055a042716cc32b00caa48ef444
Akilach01 <akilapeiris92@gmail.com>
2025-12-05 15:12:24 +0530

    implemented login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import user from "../models/user";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import user from "../models/user";
+import jwt from "jsonwebtoken";
 
 
 
@@ -33,3 +33,34 @@ export const register = async(req:Request,res:Response)=>{
 };
 
 
+export const login = async(req: Request, res: Response) => {
+    try{
+    const{email, password} =req.body;
+
+    const user = await User.findOne({email});
+    if(!user)
+        return res.status(400).json({message:"invalid email or password"});
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+        return res.status(400).json({message:"invalid email or password"});
+    
+    const token = jwt.sign({
+        id:user._id,
+        role:user.role
+    },
+    process.env.JWT_SECRET!,
+    {expiresIn: "7d"}
+  );
+
+    return res.status(200).json({message:"login success",token,user:{id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role}
+    });
+
+}catch(error){
+    return res.status(500).json({message:"server error", error});
+
+ }
+};
