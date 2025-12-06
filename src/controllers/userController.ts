6548459d4d992eb144a54fcb8836b825f1a2c3c2
Akilach01 <akilapeiris92@gmail.com>
2025-12-06 15:12:27 +0530

    updated usercontroller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,33 +1,24 @@
-import { Request, Response } from "express"
-import {User} from "../models/User"
-import {AuthRequest} from "../middleware/authMiddleware"
-import bcrypt from "bcryptjs"
+import { Request, Response } from "express";
+import User from "../models/User";
+import {AuthRequest} from "../middleware/authMiddleware";
+import bcrypt from "bcryptjs";
 
 export const updateProfile = async(req: AuthRequest, res: Response)=>{
     try {
-        const userId =req.user.sub
-        const{firstname,lastname,email,password} = req.body
+        const{name,password} = req.body;
 
-        const updates:any = {}
-
-        if(firstname)updates.firstname = firstname
-        if(lastname)updates.lastname = lastname
-        if(email)updates.email = email
-
+        const updateData: any = {name};
 
     if(password){
-        const hashedPassword = await bcrypt.hash(password,10)
-        updates.password = hashedPassword
+        updateData.Password = await bcrypt.hash(password,10);
     }
 
-    const updateUser = await User.findByIdAndUpdate(userId,updates,{new:true,}).select("-password")
+     await User.findByIdAndUpdate(req.user.id, updateData);
 
-    res.status(200).json({
-        message:"updated profile successfuly",
-        data:updateUser,
-    })
-    } catch (err:any) {
-        res.status(500).json({message:err.message})
+    res.json({
+        message:"updated profile successfuly"});
+    } catch {
+        res.status(500).json({message:"server error"});
         
     }
-}
+};
