10e45c566865d5b16436feb5c25fb22fad22cf92
Akilach01 <akilapeiris92@gmail.com>
2025-12-09 21:17:33 +0530

    updating index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-import userRouter from "./routes/userRoutes"
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
+import adminRoutes from "./routes/adminRoutes";
+import planRoutes from "./routes/planRoutes";
+import userRoutes from "./routes/userRoutes";
 
 
+dotenv.config();
 
+const app = express();
+app.use(express.json());
 
-app.use("/api/v1/user",userRouter)
+app.use(cors({ origin: "*" }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/plans", planRoutes);
+app.use("/api/admin", adminRoutes);
+
+mongoose
+.connect(process.env.MONGO_URI!)
+.then(()=>console.log("DB connected"))
+.catch((err)=>console.log("DB error:",err));
+
+app.listen(process.env.PORT || 5000,()=>
+console.log("server running")
+);
+
 
